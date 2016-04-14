97871ad8eb19f7fd74b14768d675bef0c587551e
Diegomf <mosquetero.sw@gmail.com>
2016-04-14 14:01:04 -0500

    What the fuck, Clojuregit add --allgit add --all

--- macros.clj
+++ macros.clj
@@ -8,6 +8,7 @@
 ; PROTIP -> use (macroexpand-1 '(my-macro a b c _)) to see the macro expansion.
 
 (defmacro my-and
+  "Implements and from Clojure."
   ([] true)
   ([x] x)
   ([x & y]
@@ -17,6 +18,7 @@
         temp#))))
 
 (defmacro my-or
+  "Implements or from Clojure."
   ([] nil)
   ([x] x)
   ([x & next]
@@ -26,14 +28,50 @@
         (my-or ~@next)))))
 
 (my-or)
-(my-or false :one nil :two false :three)
+(macroexpand-1 '(my-or false :one nil :two false :three))
 (my-or false false nil)
 (my-or nil nil false)
 
-(defmacro debug [e]
-  `(let [temp# ~e]
-    (do
-      (printf "debug: %s => %s%n" '~e temp#)
-      temp#)))
+(defmacro do-loop
+  "A post-test loop that implements the do-while from C and the
+  repeat-until from Pascal"
+  ([& y]
+   `(let [condition# ~(first (last y))
+          statement# ~(second (last y))]
+      (do
+        ; Execute body
+;;         (loop [i# 0
+;;                y# ~(butlast y)]
+;;           (eval (nth ~y))
+;;           (if (< i# (count y#))
+;;             (recur (inc i#) y#)
+;;             nil))
+        ; Check for condition
+        (cond
+          (= condition# :while) (if statement#
+;;                                   (do-loop ~@y)
+                                  "while"
+                                  "while nil")
+          (= condition# :until) (if (not statement#)
+;;                                   (do-loop ~@y)
+                                  "until"
+                                  "until nil"))))))
 
-(inc (debug (* 4 5)))
+(def i (atom 0))
+(macroexpand-1 '(do-loop (println @i) (swap! i inc) (:until (= @i 5))))
+(do-loop (println @i) (swap! i inc) (:until (= @i 5)))
+
+(macroexpand-1 '(do-loop (println @i) (swap! i inc) (:until (= 5 5))))
+(do-loop (println @i) (swap! i inc) (:until (= 5 5)))
+
+(defn thing
+  [a b & y]
+  (count y))
+
+(thing 'a 'b 'c)
+
+;; (defmacro debug [e]
+;;   `(let [temp# ~e]
+;;     (do
+;;       (printf "debug: %s => %s%n" '~e temp#)
+;;       temp#)))
