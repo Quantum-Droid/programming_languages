4c71240ab95584250e45d6ad1bb530bdd5abdf4a
diegomf <diego.monroy.f@gmail.com>
2016-04-30 10:16:56 -0500

    4clojure_bonus.clj file updated

--- 4clojure_bonus.clj
+++ 4clojure_bonus.clj
@@ -7,3 +7,23 @@
       (recur (merge s {(first l) keyy}) (rest l)))))
 
 (p-156 "x" [1 2 3])
+
+(defn p-25
+  [l]
+  (loop [res ()
+         l l]
+    (if (empty? l) (reverse res)
+      (if (odd? (first l))
+        (recur (cons (first l) res) (rest l))
+        (recur res (rest l))))))
+
+(p-25 [1 1 1 3])
+
+
+(defn p-27
+  [s]
+  (if (string? s)
+    (= s (apply str (reverse s)))
+    (= s (reverse s))))
+
+(p-27 '(a b a))
