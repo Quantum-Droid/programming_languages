208c4c863d57485ffe6a845b963998f6b39a6e79
diegomf <diego.monroy.f@gmail.com>
2016-05-02 09:05:21 -0500

    More 4clojure exercises

--- 4clojure_bonus.clj
+++ 4clojure_bonus.clj
@@ -27,3 +27,34 @@
     (= s (reverse s))))
 
 (p-27 '(a b a))
+
+(defn p-26
+  [n]
+  (loop [res ()
+         i 0]
+    (cond
+      (zero? n) res
+      (> i n) (reverse res)
+      (= i 0) (recur () (inc i))
+      (= i 1) (recur '(1) (inc i))
+      (= i 2) (recur '(1 1) (inc i))
+      :else (recur (cons (+ (first res) (second res)) res) (inc i)))))
+
+(p-26 6)
+
+(defn p-29
+  [s]
+  (apply str (filter #(Character/isUpperCase %) s)))
+
+(p-29 "LeL")
+
+(defn p-42
+  [n]
+  (reduce * (take n (range 1 (inc n)))))
+
+(defn p-28
+  [l]
+  (apply concat l))
+
+(p-28 '["a" ["b"] "c"])
+(p-28 '((1 2) 3 [4 [5 6]]))
